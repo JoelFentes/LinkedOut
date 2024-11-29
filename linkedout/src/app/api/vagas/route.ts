6512713b825b2f8bee64b8e79b06e944ce16bf70
Emanuele Maria <emanuuuelem@gmail.com>
2024-11-29 14:40:53 -0300

    get post candidatura e vaga

--- linkedout/src/app/api/vagas/route.ts
+++ linkedout/src/app/api/vagas/route.ts
@@ -1,4 +1,3 @@
-// pages/api/vagas.js
 import { NextResponse } from 'next/server';
 import { Vaga } from '../../../../models/vaga'; // Ajuste o caminho conforme necessário
 
@@ -8,6 +7,23 @@ export async function POST(request: Request) {
     try {
         const novaVaga = await Vaga.create(vagaData);
         return NextResponse.json(novaVaga, { status: 201 });
+    } catch (error) {
+        console.error('Erro ao criar vaga:', error);
+        return NextResponse.json({ error: 'Erro ao criar vaga' }, { status: 500 });
+    }
+}
+
+export async function GET(request: Request) {
+    const vagaData = await request.json();
+
+    try {
+        let result
+        if (vagaData.id) {
+            result = await Vaga.findByPk(vagaData.id)
+        } else {
+            result = await Vaga.findAll();
+        }
+        return NextResponse.json(result, { status: 201 });
     } catch (error) {
         console.error('Erro ao criar vaga:', error);
         return NextResponse.json({ error: 'Erro ao criar vaga' }, { status: 500 });
